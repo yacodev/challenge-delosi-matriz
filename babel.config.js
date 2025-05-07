01b425598aa5e190e29a0f74d6afbbe649e13938
yacodev <cyaco33@gmail.com>
2025-05-07 15:33:22 -0500

    fix deploy

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   presets: [
     ["@babel/preset-env", { targets: { node: "current" } }],
-    ["@babel/preset-react", { runtime: "automatic" }],
     "@babel/preset-typescript",
+    ["@babel/preset-react", { runtime: "automatic" }],
   ],
+  env: {
+    test: {
+      plugins: ["@babel/plugin-transform-modules-commonjs"],
+    },
+  },
 };
